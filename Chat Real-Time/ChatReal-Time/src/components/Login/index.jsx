9551efdf76657c6528d50eb2fe80537e63e4d8e4
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2024-06-29 00:38:59 +0700

    add handleFacebookLogin
    Xu li dang nhap facebook

--- Chat Real-Time/ChatReal-Time/src/components/Login/index.jsx
+++ Chat Real-Time/ChatReal-Time/src/components/Login/index.jsx
@@ -1,30 +1,38 @@
-import React from 'react'
-import {Row, Col, Button,Typography} from 'antd'
 
-const {Title} = Typography;
+import React from 'react';
+import { Row, Col, Button, Typography } from 'antd';
+import { auth, FacebookAuthProvider } from '../../firebase/config.js';
+import { signInWithPopup } from 'firebase/auth';
+
+const { Title } = Typography;
+
 export default function Login() {
+  const handleFacebookLogin = () => {
+    const provider = new FacebookAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div>
       <Row justify="center" style={{ height: 800 }}>
         <Col span={8}>
-        
-         <Title style = {{ textAlign: 'Center' }} level={3}>
+          <Title style={{ textAlign: 'Center' }} level={3}>
             Fun Chat
-         </Title>
-
-         <Button style={{ width: '100%' , marginBottom: 5 }}>
+          </Title>
+          <Button style={{ width: '100%', marginBottom: 5 }}>
             Login by Google
-         </Button>
-
-          <Button style={{ width: '100%' }} >
+          </Button>
+          <Button style={{ width: '100%' }} onClick={handleFacebookLogin}>
             Login by Facebook
           </Button>
-
         </Col>
-        
       </Row>
-      
-      
     </div>
-  )
+  );
 }
--- Chat Real-Time/ChatReal-Time/src/firebase/config.js
+++ Chat Real-Time/ChatReal-Time/src/firebase/config.js
@@ -1,21 +1,27 @@
-import firebase from 'firebase/compat/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, FacebookAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-import 'firebase/analytics';
-import 'firebase/auth';
-import 'firebase/storage';
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCU6r3gyS5vJffe5Hr3VTe8R0by5ob3CGo",
-    authDomain: "chat-app-25ce6.firebaseapp.com",
-    projectId: "chat-app-25ce6",
-    storageBucket: "chat-app-25ce6.appspot.com",
-    messagingSenderId: "762103832300",
-    appId: "1:762103832300:web:275197b42a3b41b2af0fc9",
-    measurementId: "G-35YJ6QDKSE"
+  apiKey: "AIzaSyCU6r3gyS5vJffe5Hr3VTe8R0by5ob3CGo",
+  authDomain: "chat-app-25ce6.firebaseapp.com",
+  projectId: "chat-app-25ce6",
+  storageBucket: "chat-app-25ce6.appspot.com",
+  messagingSenderId: "762103832300",
+  appId: "1:762103832300:web:275197b42a3b41b2af0fc9",
+  measurementId: "G-35YJ6QDKSE"
 };
 
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+// Export the necessary instances
+export { db, auth, storage, FacebookAuthProvider };
